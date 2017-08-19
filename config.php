25bdd087fb70a1ac44ab6d97b9933ce784b33808
HontarSoftware <31164346+HontarSoftware@users.noreply.github.com>
2017-08-19 19:09:21 +0200

    Erstellen der Konfigurationsdatei

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Konfigurationsdatei
+ * 
+ * @author		Philipp Bornemann
+ * @copyright	2017 HontarSoftware
+ * @version		1.0
+ */
+
+$hf_version = '1.0.0 Beta 1';
+$hf_version_code = 1000;
+
+$root_path = dirname(__FILE__);
+
+$db_host = '';
+$db_name = '';
+$db_username = '';
+$db_password = '';
